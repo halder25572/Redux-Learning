05aaff46600fe3182abd202029a7d70f70cec8eb
halder25572 <halder25572.com>
2025-11-25 03:30:37 +0600

    Multiple reducers

--- index3.js
+++ index3.js
@@ -1,23 +1,48 @@
-const { createStore } = require("@reduxjs/toolkit");
+const { createStore, combineReducers } = require("@reduxjs/toolkit");
 
 // products constants
 const GET_PRODUCTS = 'GET_PRODUCTS';
 const ADD_PRODUCT = 'ADD_PRODUCT';
 
+// cart constants
+const GET_CART_ITEMS = 'GET_CART_ITEMS';
+const ADD_CART_ITEM = 'ADD_CART_ITEM';
+
+// cart states
+const initialCartState = {
+    carts: ['Salt'],
+    numberofCards: 1,
+}
+// products states
 const initialProductState = {
     products: ['Sugar', 'Salt'],
     numberofProducts: 2,
 }
 
+// product action
 const getProduct = () => {
     return {
         type: GET_PRODUCTS
     }
 }
+// product action
 const addProduct = (product) => {
     return {
         type: ADD_PRODUCT,
         payload: product
+    }
+}
+
+// cart action
+const getCart = () => {
+    return {
+        type: GET_CART_ITEMS
+    }
+}
+// cart action
+const addCart = () => {
+    return {
+        type: ADD_CART_ITEM
     }
 }
 
@@ -38,12 +63,36 @@ const productReducer = (state = initialProductState, action) => {
             return state;
     }
 }
+// cartReducer
+const cartReducer = (state = initialCartState, action) => {
+    switch (action.type) {
+        case GET_CART_ITEMS:
+            return {
+                ...state
+            }
+        case ADD_CART_ITEM:
+            return {
+                carts: [...state.carts, action.payload],
+                numberofCards: state.numberofCards + 1
+            }
 
-// store
-const store = createStore(productReducer);
+        default:
+            return state;
+    }
+}
 
+const rootReducer = combineReducers({
+    productR: productReducer,
+    cardR: cartReducer
+})
+
+// store for product
+const store = createStore(rootReducer);
+
+// for product
 store.subscribe(() => {
     console.log(store.getState());
 });
 
-store.dispatch(addProduct('Apple'));
+// store.dispatch(addProduct('Apple'));
+store.dispatch(getCart());

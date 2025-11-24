3de7da06fbae777371ae3699df42c748200e9720
halder25572 <halder25572.com>
2025-11-25 05:30:32 +0600

    learn redux fetch api

--- index3.js
+++ index3.js
@@ -1,18 +1,12 @@
-const { createStore, combineReducers } = require("@reduxjs/toolkit");
+// const { createStore,  } = require("@reduxjs/toolkit");
+
+const {createStore, applyMiddleware} = require("redux");
+const { default: logger } = require("redux-logger");
 
 // products constants
 const GET_PRODUCTS = 'GET_PRODUCTS';
 const ADD_PRODUCT = 'ADD_PRODUCT';
 
-// cart constants
-const GET_CART_ITEMS = 'GET_CART_ITEMS';
-const ADD_CART_ITEM = 'ADD_CART_ITEM';
-
-// cart states
-const initialCartState = {
-    carts: ['Salt'],
-    numberofCards: 1,
-}
 // products states
 const initialProductState = {
     products: ['Sugar', 'Salt'],
@@ -33,19 +27,6 @@ const addProduct = (product) => {
     }
 }
 
-// cart action
-const getCart = () => {
-    return {
-        type: GET_CART_ITEMS
-    }
-}
-// cart action
-const addCart = () => {
-    return {
-        type: ADD_CART_ITEM
-    }
-}
-
 // productReducer
 const productReducer = (state = initialProductState, action) => {
     switch (action.type) {
@@ -63,31 +44,9 @@ const productReducer = (state = initialProductState, action) => {
             return state;
     }
 }
-// cartReducer
-const cartReducer = (state = initialCartState, action) => {
-    switch (action.type) {
-        case GET_CART_ITEMS:
-            return {
-                ...state
-            }
-        case ADD_CART_ITEM:
-            return {
-                carts: [...state.carts, action.payload],
-                numberofCards: state.numberofCards + 1
-            }
-
-        default:
-            return state;
-    }
-}
-
-const rootReducer = combineReducers({
-    productR: productReducer,
-    cardR: cartReducer
-})
 
 // store for product
-const store = createStore(rootReducer);
+const store = createStore(productReducer, applyMiddleware(logger));
 
 // for product
 store.subscribe(() => {
---
+++ index4.js
@@ -0,0 +1,83 @@
+// async actions - api calling
+// api url - https://jsonplaceholder.typicode.com/todos
+// middleware - redux-thunk
+// axios api
+const { createStore, applyMiddleware } = require("redux");
+const axios = require("axios");
+const thunk = require("redux-thunk").thunk;  // <-- FIXED: correct middleware export
+
+// constants
+const GET_TODOS_REQUEST = 'GET_TODOS_REQUEST';
+const GET_TODOS_SUCCESS = 'GET_TODOS_SUCCESS';
+const GET_TODOS_FAILED = 'GET_TODOS_FAILED';
+
+const API_URL = 'https://jsonplaceholder.typicode.com/todos';
+
+// initial state
+const initialTodosState = {
+    todos: [],
+    isLoading: false,
+    error: null
+};
+
+// actions
+const getTodosRequest = () => ({
+    type: GET_TODOS_REQUEST
+});
+
+const getTodosSuccess = (todos) => ({
+    type: GET_TODOS_SUCCESS,
+    payload: todos
+});
+
+const getTodosFailed = (error) => ({
+    type: GET_TODOS_FAILED,
+    payload: error
+});
+
+// reducer
+const todoReducer = (state = initialTodosState, action) => {
+    switch (action.type) {
+        case GET_TODOS_REQUEST:
+            return { ...state,
+                 isLoading: true };
+
+        case GET_TODOS_SUCCESS:
+            return { ...state,
+                 isLoading: false,
+                 todos: action.payload };
+
+        case GET_TODOS_FAILED:
+            return { ...state,
+                 isLoading: false,
+                  error: action.payload };
+
+        default:
+            return state;
+    }
+};
+
+// async action creator
+const fetchData = () => {
+    return (dispatch) => {
+
+        dispatch(getTodosRequest());
+
+        axios.get(API_URL)
+            .then(res => {
+                dispatch(getTodosSuccess(res.data));
+            })
+            .catch(error => {
+                dispatch(getTodosFailed(error.message));
+            });
+    };
+};
+
+// store
+const store = createStore(todoReducer, applyMiddleware(thunk));
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch(fetchData());
